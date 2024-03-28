39920d9d57f544b662147a27d7bc1d88d622914b
metrokacigamer <metreveliraph@gmail.com>
2024-03-28 13:19:51 +0400

    Revert "init"
    This reverts commit 7bd8159ddbf20a299c294c4155a123e2ff530dc3.

--- ShoppingApp/ShoppingApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShoppingApp.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
